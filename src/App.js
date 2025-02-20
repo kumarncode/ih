231459dddd905289eed618ed809d0adf1a0b8dae
kumarncode <103235288+kumarncode@users.noreply.github.com>
2025-02-20 17:47:16 +0530

    latest

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import Contact from './components/pages/Contact';
 import Portfolio from './components/pages/Portfolio';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import ScrollToTop from './components/common/ScrollToTop';
 function App() {
   return (
    <>
+   <ScrollToTop />
    <Header />
    
    <Routes>
